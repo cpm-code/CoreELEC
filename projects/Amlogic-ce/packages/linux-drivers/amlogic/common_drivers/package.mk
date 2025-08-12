043c0abc3283d5f958978c7f3f7bce8cb4eda103
Peter <peter.vicman@gmail.com>
2025-08-12 09:54:28 +0200

    common_drivers: bump package to 50ce8fc2
    allow mounting EROFS with AMFC

--- projects/Amlogic-ce/packages/linux-drivers/amlogic/common_drivers/package.mk
+++ projects/Amlogic-ce/packages/linux-drivers/amlogic/common_drivers/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2024-present Team CoreELEC (https://coreelec.org)
 
 PKG_NAME="common_drivers"
-PKG_VERSION="993c1536025b34f8d7f387d3c1c68432fa22ebec"
-PKG_SHA256="effc5abefe16f0bef0276a484809a5408fe53707a1ccfbb06f79a4935360dcd4"
+PKG_VERSION="50ce8fc277c7415451eb012d88f4ca5578e23fe1"
+PKG_SHA256="e3330a44488b1730d9365642ca54b040f321721e7d9405a61248b8b15ee6a578"
 PKG_LICENSE="GPL-2.0+ OR MIT"
 PKG_SITE="https://coreelec.org"
 PKG_URL="https://github.com/CoreELEC/common_drivers/archive/${PKG_VERSION}.tar.gz"
