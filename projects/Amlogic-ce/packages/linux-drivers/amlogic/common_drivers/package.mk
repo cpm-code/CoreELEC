da1fe6867630c1206a1cc2a1b71eae856fffddf6
Portisch <hugo.portisch@yahoo.de>
2025-09-01 15:36:10 +0200

    Amlogic-no: update linux to 5.15.170

--- projects/Amlogic-ce/packages/linux-drivers/amlogic/common_drivers/package.mk
+++ projects/Amlogic-ce/packages/linux-drivers/amlogic/common_drivers/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2024-present Team CoreELEC (https://coreelec.org)
 
 PKG_NAME="common_drivers"
-PKG_VERSION="4dfd536c4908037aceea58b0302bfaed0f93fbfa"
-PKG_SHA256="6ecf5ed001ce0a94209c985dabae236c8497cc95674c334a986c17b2b3241a18"
+PKG_VERSION="051c2459571b13977cba2e2be1e198198d053727"
+PKG_SHA256="8d9666a4fc8f77eadc9ec2b7a4b5002612cac5b792d278b4ec6e0fc604cbfd00"
 PKG_LICENSE="GPL-2.0+ OR MIT"
 PKG_SITE="https://coreelec.org"
 PKG_URL="https://github.com/CoreELEC/common_drivers/archive/${PKG_VERSION}.tar.gz"
