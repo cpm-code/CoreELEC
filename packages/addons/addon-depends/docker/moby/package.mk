b8b9ae5657896db22a4c3209d7039069f3750ed3
Peter <peter.vicman@gmail.com>
2025-11-29 14:51:28 +0100

    moby: fix wrong permissions
    rm: cannot remove '/home/docker/build_dir/CoreELEC-22/build.CoreELEC-Amlogic-no.aarch64-22/build/moby-29.0.4/.gopath/pkg/mod/golang.org/toolchain@v0.0.1-go1.24.0.linux-amd64/CONTRIBUTING.md': Permission denied

--- packages/addons/addon-depends/docker/moby/package.mk
+++ packages/addons/addon-depends/docker/moby/package.mk
@@ -43,6 +43,12 @@ make_target() {
   ${GOLANG} build -mod=mod -modfile=go.mod -v -o bin/dockerd -a -tags "${PKG_MOBY_BUILDTAGS}" -ldflags "${LDFLAGS}" ./cmd/dockerd
 }
 
+post_make_target() {
+  # fix wrong permissions which prevents folder to be removed without sudo
+  find ${PKG_BUILD} -type f -perm 0444 -exec chmod 0644 {} +
+  find ${PKG_BUILD} -type d -perm 0555 -exec chmod 0755 {} +
+}
+
 makeinstall_target() {
   :
 }
