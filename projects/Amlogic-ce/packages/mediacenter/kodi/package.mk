d30d4c98304c87514047c1d19c930018cea27999
Portisch <hugo.portisch@yahoo.de>
2025-11-30 17:15:57 +0100

    kodi: bump package to ba737941

--- projects/Amlogic-ce/packages/mediacenter/kodi/package.mk
+++ projects/Amlogic-ce/packages/mediacenter/kodi/package.mk
@@ -4,7 +4,7 @@
 # Copyright (C) 2022-present Team CoreELEC (https://coreelec.tv)
 
 PKG_NAME="kodi"
-PKG_VERSION="34c8ca3594c7223065c4116f2296c0bb08b23e4d"
+PKG_VERSION="ba73794191c0234978f70d48c21e4584f741933f"
 PKG_SHA256=""
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.kodi.tv"
