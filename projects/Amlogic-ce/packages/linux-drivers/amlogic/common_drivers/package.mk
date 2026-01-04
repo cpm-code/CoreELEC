f8c8091b3a435ea9ffdd52be03a476b569db5a36
Peter <peter.vicman@gmail.com>
2026-01-04 18:08:29 +0100

    common_drivers: bump package to f0bbbf32
    led: simplify SK9822 driver and add sysfs interface
    
    ugoos am9:~ # cat /sys/devices/platform/led/colors/active
    active color is 1 00 30 00
    
    ugoos am9:~ # echo "2 10 30 00" >/sys/devices/platform/led/colors/active
    
    ugoos am9:~ # dmesg | tail -n 1
    [  508.058502] sk9822-spi led: active color stored as 2 10 30 00
    
    ugoos am9:~ # cat /sys/devices/platform/led/colors/active
    active color is 2 10 30 00

--- projects/Amlogic-ce/packages/linux-drivers/amlogic/common_drivers/package.mk
+++ projects/Amlogic-ce/packages/linux-drivers/amlogic/common_drivers/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2024-present Team CoreELEC (https://coreelec.org)
 
 PKG_NAME="common_drivers"
-PKG_VERSION="2c4ead2f6efa678d1eea2666402fdf48961edcf7"
+PKG_VERSION="f0bbbf32574ebcc5f9303a2762b793ad30016a04"
 PKG_SHA256=""
 PKG_LICENSE="GPL-2.0+ OR MIT"
 PKG_SITE="https://coreelec.org"
