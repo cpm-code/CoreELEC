936b6149420c65105a960136306613f0ed8a0df9
cpm <162544069+cpm-code@users.noreply.github.com>
2026-03-31 08:17:51 +0800

    libdovi: build from source with cargo and cbindgen

--- projects/Amlogic-ce/packages/multimedia/libdovi/package.mk
+++ projects/Amlogic-ce/packages/multimedia/libdovi/package.mk
@@ -5,51 +5,61 @@ PKG_NAME="libdovi"
 PKG_VERSION="3ec01dccbf5a74dfc7c58e3864029187b715344f"
 PKG_SITE="https://github.com/quietvoid/dovi_tool"
 PKG_DEPENDS_TARGET="toolchain"
-if [ "${BUILD_FROM_SRC}" = "yes" ]; then
-  PKG_SHA256="9483c81af5ca34ad8081ee71b0c31bdb92114d29bdcb664774ede4071c3fcea2"
-  PKG_URL="https://github.com/quietvoid/dovi_tool/archive/${PKG_VERSION}.tar.gz"
-  PKG_DEPENDS_TARGET+=" cargo-c:host"
-else
-  case "${TARGET_ARCH}" in
-    "arm")
-      PKG_SHA256="e034776d5e8fbad6f22092cd06c7a94252962467de7ae298cc98f2823281f8fa"
-      ;;
-    "aarch64")
-      PKG_SHA256="6807b37cb55d49953196cc150941e6fe26c7deb42e25d266d82bb37b29e3c612"
-      ;;
-  esac
-  PKG_SOURCE_NAME="${PKG_NAME}-${ARCH}-${PKG_VERSION}.tar.xz"
-  PKG_URL="https://sources.coreelec.org/${PKG_SOURCE_NAME}"
-fi
+PKG_SHA256="9483c81af5ca34ad8081ee71b0c31bdb92114d29bdcb664774ede4071c3fcea2"
+PKG_URL="https://github.com/quietvoid/dovi_tool/archive/${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET+=" cargo:host cbindgen:host"
 PKG_LICENSE="MIT"
 PKG_LONGDESC="dovi_tool is a CLI tool combining multiple utilities for working with Dolby Vision."
 PKG_TOOLCHAIN="manual"
 
-if [ "${BUILD_FROM_SRC}" = "yes" ]; then
 pre_make_target() {
-  CARGO_BASE_OPTS="--manifest-path ${PKG_BUILD}/dolby_vision/Cargo.toml \
-                   --target ${TARGET_NAME}"
-  CARGO_BUILD_OPTS="--library-type staticlib \
+  CARGO_CRATE_DIR="${PKG_BUILD}/dolby_vision"
+  CARGO_TARGET="${RUST_TARGET_SPEC:-${TARGET_NAME}}"
+  CARGO_TARGET_DIR_NAME="${TARGET_NAME}"
+  CARGO_STATICLIB="${PKG_BUILD}/.${TARGET_NAME}/target/${CARGO_TARGET_DIR_NAME}/release/libdolby_vision.a"
+  CARGO_HEADER="${PKG_BUILD}/.${TARGET_NAME}/target/${CARGO_TARGET_DIR_NAME}/release/rpu_parser.h"
+  CARGO_PKG_VERSION="$(sed -n 's/^version = "\(.*\)"/\1/p' ${CARGO_CRATE_DIR}/Cargo.toml | head -n 1)"
+  CARGO_FETCH_OPTS="--manifest-path ${CARGO_CRATE_DIR}/Cargo.toml \
+                    --target ${CARGO_TARGET}"
+  CARGO_BASE_OPTS="--manifest-path ${CARGO_CRATE_DIR}/Cargo.toml \
+                   --target ${CARGO_TARGET} \
+                   --features capi"
+  CARGO_BUILD_OPTS="--lib \
                     --profile release \
-                    --prefix /usr
                     ${CARGO_BASE_OPTS}"
+
+  if ! grep -q '^crate-type = ' ${CARGO_CRATE_DIR}/Cargo.toml; then
+    sed -i '/^\[lib\]$/a crate-type = ["rlib", "staticlib"]' ${CARGO_CRATE_DIR}/Cargo.toml
+  fi
 }
 
 make_target() {
-  cargo fetch ${CARGO_BASE_OPTS}
-  cargo cbuild ${CARGO_BUILD_OPTS}
+  cargo fetch ${CARGO_FETCH_OPTS}
+  cargo build ${CARGO_BUILD_OPTS}
+  ${TOOLCHAIN}/bin/cbindgen ${CARGO_CRATE_DIR} \
+    --config ${CARGO_CRATE_DIR}/cbindgen.toml \
+    --crate dolby_vision \
+    --output ${CARGO_HEADER}
 }
 
 makeinstall_target() {
-  cargo cinstall ${CARGO_BUILD_OPTS} --destdir ${SYSROOT_PREFIX}
-  cargo cinstall ${CARGO_BUILD_OPTS} --destdir ${INSTALL}
-}
-else
-make_target() {
-  cp -PR * ${SYSROOT_PREFIX}
-}
+  for destdir in ${SYSROOT_PREFIX} ${INSTALL}; do
+    mkdir -p ${destdir}/usr/include/libdovi ${destdir}/usr/lib/pkgconfig
 
-makeinstall_target() {
-  : #
+    cp ${CARGO_STATICLIB} ${destdir}/usr/lib/libdovi.a
+    cp ${CARGO_HEADER} ${destdir}/usr/include/libdovi/rpu_parser.h
+
+    cat >${destdir}/usr/lib/pkgconfig/dovi.pc <<EOF
+prefix=/usr
+exec_prefix=${prefix}
+libdir=${exec_prefix}/lib
+includedir=${prefix}/include
+
+Name: dovi
+Description: Dolby Vision metadata parsing and writing library
+Version: ${CARGO_PKG_VERSION}
+Libs: -L${libdir} -ldovi
+Cflags: -I${includedir}
+EOF
+  done
 }
-fi
